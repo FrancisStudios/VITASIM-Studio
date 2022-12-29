8553c56acb256a53fafec8e9b854dbd64a663f3e
unknown <vlfmap@gmail.com>
2022-12-29 14:36:47 +0100

    flash screen added

--- main.js
+++ main.js
@@ -5,11 +5,12 @@ let mainWindow;
 
 function createWindow () {
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 600,
+    height: 400,
     webPreferences: {
       nodeIntegration: true
-    }
+    },
+    frame: false
   })
 
   mainWindow.loadFile('index.html');
